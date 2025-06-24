5dd4fd588b016daf9f8e536837bd89aacd90c757
paidforby <20746091+paidforby@users.noreply.github.com>
2025-06-24 15:28:03 -0400

    Add update-download.sh and reformat download.html
    Minor reformat of download links to make them work with
    update-download.sh script.

---
+++ update-download.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Usage: ./update-download.sh VERSION DATE {CHANNEL}
+# Update download.html page with the given version + date of Ceno apk.
+
+version=$1
+date=$2
+channel="${3:-release}"
+
+validVersion=$([[ "$version" =~ ^([0-9]|[1-9][0-9]).([0-9]|[1-9][0-9]).([0-9]|[1-9][0-9])$ ]] && echo true)
+validDate=$([[ "$date" =~ ^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])_[0-9]{4}$ ]] && echo true)
+
+if [[ "$validVersion" = true && "$validDate" = true ]]; then
+    sed -Ei \
+        -e "s#<p><strong>Ceno Browser .*</strong></p>#<p><strong>Ceno Browser $version</strong></p>#g" \
+        -e "s#"https://ceno-download.s3.amazonaws.com/ceno/v.*/ceno-arm64-v8a-.*.apk"#"https://ceno-download.s3.amazonaws.com/ceno/v$version/ceno-arm64-v8a-$channel-$version-$date.apk"#g" \
+        -e "s#"https://ceno-download.s3.amazonaws.com/ceno/v.*/ceno-armeabi-v7a-.*.apk"#"https://ceno-download.s3.amazonaws.com/ceno/v$version/ceno-armeabi-v7a-$channel-$version-$date.apk"#g" \
+        -e "s#"https://ceno-download.s3.amazonaws.com/ceno/v.*/ceno-x86_64-.*.apk"#"https://ceno-download.s3.amazonaws.com/ceno/v$version/ceno-x86_64-$channel-$version-$date.apk"#g" \
+        -e "s#"https://ceno-download.s3.amazonaws.com/ceno/v.*/sha256sums.txt.asc"#"https://ceno-download.s3.amazonaws.com/ceno/v$version/sha256sums.txt.asc"#g" \
+        -e "s#"https://ceno-download.s3.amazonaws.com/ceno/v.*/sha256sums.txt"#"https://ceno-download.s3.amazonaws.com/ceno/v$version/sha256sums.txt"#g" \
+        ./_en.dev/download.html
+else
+    if [[ "$validVersion" != true ]]; then
+        echo "Version formatted incorrectly, ${version} should be in X.X.X format"
+    fi
+    if [[ "$validDate" != true ]]; then
+        echo "Date formatted incorrectly, ${date} should be in YYYY-MM-DD_HHMM format"
+    fi
+fi
